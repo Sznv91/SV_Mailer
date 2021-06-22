7e287080f99e10c0c953ae96e678bc00f2ff3c06
Mihail Sazonov <sazonov91@gmail.com>
2021-06-22 17:42:37 +0300

    Выполнено завершение работ над заготовкой.

--- app/src/main/java/ru/softvillage/mailer_test/ui/fragmet/SendReceipt.java
+++ app/src/main/java/ru/softvillage/mailer_test/ui/fragmet/SendReceipt.java
@@ -1,49 +1,27 @@
 package ru.softvillage.mailer_test.ui.fragmet;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import ru.softvillage.mailer_test.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SendReceipt#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SendReceipt extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_PARAM1 = "startPositionOnRecyclerView";
+    private int startPositionOnRecyclerView;
 
     public SendReceipt() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SendReceipt.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SendReceipt newInstance(String param1, String param2) {
+    public static SendReceipt newInstance(int startPositionOnRecyclerView) {
         SendReceipt fragment = new SendReceipt();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM1, startPositionOnRecyclerView);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,15 +30,15 @@ public class SendReceipt extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            startPositionOnRecyclerView = getArguments().getInt(ARG_PARAM1);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_send_receipt, container, false);
     }
 }
+
+
--- app/src/main/java/ru/softvillage/mailer_test/ui/tabs/PagerAdapter.java
+++ app/src/main/java/ru/softvillage/mailer_test/ui/tabs/PagerAdapter.java
@@ -20,9 +20,9 @@ public class PagerAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         switch (position) {
             case 0:
-                return AllReceipt.newInstance("0", "0");
+                return AllReceipt.newInstance(0);
             case 1:
-                return SendReceipt.newInstance("0", "0");
+                return SendReceipt.newInstance(0);
         }
         return null;
     }
